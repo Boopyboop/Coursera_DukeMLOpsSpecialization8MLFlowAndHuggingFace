35dcbd6491e95cdcdce8974030cad6e408423e17
BoopyBoop <manon.vlieghe@gmail.com>
2025-09-22 15:29:55 +0200

    improved logging

--- Module1_MLFlow/ocr_mlops/src/ocr/register_model.py
+++ Module1_MLFlow/ocr_mlops/src/ocr/register_model.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", required=True, help="Registered model name")
     args = parser.parse_args()
 
-    mlflow.register_model(f"runs:/{args.run_id}/model", args.model_name)
-    print(f"✅ Registered model {args.model_name} from run {args.run_id}")
+    mlflow.register_model(f"runs:/{args.run_id}/ocr_model", args.model_name)
+    print(f"Registered model {args.model_name} from run {args.run_id} (artifact_path=ocr_model)")
+
